280ab4e93bd979ade074ed8330ca2e63e004dc1a
Dhruv Rathod <128411388+spicy-potato-cat@users.noreply.github.com>
2025-04-16 20:46:45 +0530

    .

---
+++ src/main/java/root/oop_project/OOP_Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package root.oop_project;
+
+/**
+ *
+ * @author dhruv
+ */
+public class OOP_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
